574eed66f6dd887d97c0cad8b1831704c03b843b
dw1219 <ymg886@naver.com>
2021-08-26 14:07:58 +0900

    ox quiz upload first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-//import './App.css';
+import React, { useRef } from 'react'
+import './App.css';
 //import MainPage from "./views/MainPage/MainPage.js";
 import { Route, Switch } from "react-router-dom";
 import MainPage from './views/MainPage';
+import QuizPage from './views/QuizPage/QuizPage';
+import bgmStart from '../resources/picture/bgmStart.png';
+import bgmPause from '../resources/picture/bgmPause.png';
+import music from "../resources/music/HappyMistake.mp3"
+
 
 function App() {
+
+  const audioRef = useRef(null);
+
+  const handlePlay = () => {
+    audioRef.current.play();
+  };
+
+  const handlePause = () => {
+    audioRef.current.pause();
+  };
+
   return (
     <div className="App">
+      <div class="bgmOnOff">
+        <audio src={music} ref={audioRef} loop>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+        <img src={bgmStart} class="bgmStart" alt="" onClick={handlePlay}/>
+        <img src={bgmPause} class="bgmPause" alt="" onClick={handlePause}/>
+      </div>
       <Route exact path="/" component={MainPage} />
+      <Route exact path="/quiz" component={QuizPage} />
+      
     </div>
   );
 }
--- src/components/views/MainPage.js
+++ src/components/views/MainPage.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import './MainPage.css'
+import imgTitle from '../../resources/picture/oxQuizTitle.png';
+import imgH1 from '../../resources/picture/h1.png';
+import imgH2 from '../../resources/picture/h2.png';
+import btnStart from '../../resources/picture/btnStart3.png';
+import imgCloudMain from '../../resources/picture/cloudMain.png'
+import imgHill from '../../resources/picture/hill3.png'
+import { Link } from 'react-router-dom';
+import lottie from 'lottie-web';
+
 
 function MainPage() {
-    return (
-        <div>
-            MainPage
-        </div>
-    )
+
+  const container1 = useRef(null)
+  const container2 = useRef(null)
+  const container3 = useRef(null)
+
+    useEffect(() => {
+    lottie.loadAnimation({
+      container: container1.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../resources/svg/lottie_gift.json'),
+    })
+    lottie.loadAnimation({
+      container: container2.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../resources/svg/lottie_play1.json'),
+    })
+    lottie.loadAnimation({
+      container: container3.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../../resources/svg/lottie_play2.json'),
+    })
+  }, [])
+
+  return (
+    <div class="oxMain">
+      <div class="LottieGift" ref={container1} />
+      <div class="wrapImgTitle">
+        <img src={imgTitle} class="imgTitle" alt=""/>
+      </div>
+      <div class="wrapImgH1">
+        <img src={imgH1} class="imgH1" alt=""/>
+      </div>
+      <div class="wrapImgH2">
+        <img src={imgH2} class="imgH2" alt=""/>
+      </div>
+      <div class="wrapButton">
+        <img src={btnStart} class="btnStart" alt=""/>
+      </div>
+      <div class="wrapOtherImgs">
+        <img src={imgCloudMain} class="imgCloudMain" alt="" />
+        <img src={imgHill} class="imgHill" alt="" />
+      </div>
+      <div class="LottiePlay1" ref={container2} />
+      <div class="LottiePlay2" ref={container3} />
+    </div>
+        
+  )
 }
 
 export default MainPage
